7b868f2c148d53e4c0ceb63e198fa2d7d3e74059
shablovskyi_illia <shablovskyiillia@gmail.com>
2023-06-16 19:01:53 +0300

    create/update index.js and script.js

---
+++ script.js
@@ -0,0 +1,46 @@
+// Реалізувати рекурсивну функцію, яка зводить число в ступінь.
+
+// Число, яке треба піднести до ступеню, передається як перший аргумент у функції;
+// Ступінь передається як другий аргумент у функцію pow (num, degree).
+
+const num = parseInt(prompt("Введіть число"));
+const degree = parseInt(prompt("Введіть ступінь"));
+
+if (
+  num === "" ||
+  num === null ||
+  num === isNaN ||
+  degree === "" ||
+  degree === null ||
+  degree === isNaN
+) {
+  alert("Щось пішло не так");
+}
+
+// TODO: Реалізація за допомогою function
+
+// function pow(num, degree){
+//   if (degree === 1){
+//     return num
+//   }
+//   if(degree === 0){
+//     return 1
+//   }else {
+//     return num * pow(num, degree - 1)
+//   }
+// }
+
+// TODO: Реалізація за допомогою ()=>{}
+
+const power = (num, degree) => {
+  if (degree === 1) {
+    return num;
+  }
+  if (degree === 0) {
+    return 1;
+  } else {
+    return num * power(num, degree - 1);
+  }
+};
+
+console.log(power(num, degree));
